a01f2707c388255b51e68c471809ac9b9e642508
trishala8 <trish2talk@gmail.com>
2017-04-12 16:20:31 -0600

    Add files via upload

---
+++ views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Album
+
+def index(request):
+    all_albums = Album.objects.all()
+    context = {'all_albums': all_albums}
+    return render(request, 'music/index.html', context)
+
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2>details for album_id: " +str(album_id) + "</h2>")
+
